443b15cf1a4bfd3fdb73851759f79bea893db030
Andrew <njayaandrew@gmail.com>
2018-10-19 09:26:15 +0300

    [chore 161335128] Created an endpoint to display sales records [finishes #161335128]

--- test_admin_sales.py
+++ test_admin_sales.py
@@ -10,11 +10,14 @@ def cli_ent():
 def test_get(cli_ent):
     response=cli_ent.get('/api/v1/admin/sales')
     data=json.loads(response.data)
-    assert data== {'Sales':[{'sale_id':1,'attendant_id':2,'product_id':2,'date':'Oct 10, 2018 5:00 P.M','transanction_cost':59.98},
-                  {'sale_id':2,'attendant_id':1,'product_id':1,'date':'Oct 10, 2018 6:25 P.M','transanction_cost':100},
-                  {'sale_id':3,'attendant_id':3,'product_id':3,'date':'Oct 10, 2018 7:30 P.M','transanction_cost':60}]}
+    assert data== {'Sales':[{"product name": "Logitech keyboard","Date sold": "Oct 10, 2018 5:00 P.M","Total cost": 120,"Quantity bought": 2,},
+                            {"product name": "Iphone 6 plus","Date sold": "Oct 10, 2018 6:25 P.M","Total cost": 600,"Quantity bought": 1},
+                            {"product name": "Subwoofer Desktop Speakers","Date sold": "Oct 10, 2018 7:30 P.M","Total cost": 20,"Quantity bought": 1}]}
+
 
 def test_get_sale_record(cli_ent):
     response=cli_ent.get('/api/v1/admin/sales/'+str(2))
     data=json.loads(response.data)
-    assert data=={'Sale':{'sale_id':2,'attendant_id':1,'product_id':1,'date':'Oct 10, 2018 6:25 P.M','transanction_cost':100}}
+    assert data=={'Sale':{'product name':'Iphone 6 plus',
+    'Date sold':'Oct 10, 2018 6:25 P.M',
+    'Total cost':600,'Quantity bought':1}}
