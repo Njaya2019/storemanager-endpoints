c3b4187acc5497e35537d308baf6ebec7b2f0ec7
Andrew <njayaandrew@gmail.com>
2018-10-19 11:08:37 +0300

    [chore #161336609] created an attendant endpoint to create a sale record and can inquire a sale record [finishes #161336609]

--- test_storeattendant.py
+++ test_storeattendant.py
@@ -7,15 +7,18 @@ def cli_ent():
     return client
 
 def test_post(cli_ent):
-    response=cli_ent.post('/api/v1/attendant/sales', data=json.dumps(dict(attendant_id=1,
-    product_id=2,date='Oct 9, 2018 2:52 P.M',transanction_cost=80)), content_type="application/json")
+    response=cli_ent.post('/api/v1/attendant/sales', data=json.dumps(dict(
+    product_name='Iphone 6 plus',quantity=1
+    )), content_type="application/json")
     data=json.loads(response.data)
     assert "The transaction has been completed" in data["message"]
 
 def test_get_sale_record(cli_ent):
     response=cli_ent.get('/api/v1/attendant/sales/'+str(2))
     data=json.loads(response.data)
-    assert data=={'Sale':{'sale_id':2,'attendant_id':1,'product_sold':1,'date':'Oct 9, 2018 9:00 A.M','transanction_cost':100}}
+    assert data=={'Sale':[{'product name':'Iphone 6 plus',
+                        'Date sold':'Oct 10, 2018 6:25 P.M',
+                        'Total cost':600,'Quantity bought':1}]}
 
 
 
